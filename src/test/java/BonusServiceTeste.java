f47754a820c82902bcf2658128a6225d2b56033b
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-15 17:50:07 -0300

    Executando teste (Assertions.assertEquals)

---
+++ src/test/java/BonusServiceTeste.java
@@ -0,0 +1,39 @@
+
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import br.com.alura.tdd.service.BonusService;
+
+public class BonusServiceTeste {
+    
+    @Test
+    void bonusDeveriaSerZeroParaFuncionariosComSalarioMuitoAlto(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Pedro", LocalDate.now(), new BigDecimal ("25000")));
+
+        Assertions.assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    void bonusDeveriaSerDezPorCentoDoSalario(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Pedro", LocalDate.now(), new BigDecimal ("2500")));
+
+        Assertions.assertEquals(new BigDecimal("250.00"), bonus);
+    }
+
+    @Test
+    void bonus(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.calcularBonus(new Funcionario("Pedro", LocalDate.now(), new BigDecimal ("10000")));
+
+        Assertions.assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+
+
+}
