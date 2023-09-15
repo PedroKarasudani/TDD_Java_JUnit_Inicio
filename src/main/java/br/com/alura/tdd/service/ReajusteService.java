fa56f4d05b84d89b2b700068eb00e9161d237cb7
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-15 19:17:13 -0300

    TDD - Refatoracao, aplicando o padrao de projeto (PATTERNS STRATEGY)

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,20 +8,10 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        if (desempenho == Desempenho.A_DESEJAR){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
+        
+            BigDecimal percentual = desempenho.percentualReajuste();
+            BigDecimal reajuste = funcionario.getSalario().multiply(percentual);
             funcionario.reajusteSalario(reajuste);
-        }
-
-        if (desempenho == Desempenho.BOM){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-            funcionario.reajusteSalario(reajuste);
-        } 
-
-        if (desempenho == Desempenho.OTIMO){
-            BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-            funcionario.reajusteSalario(reajuste);
-        }
     }
 
 }
