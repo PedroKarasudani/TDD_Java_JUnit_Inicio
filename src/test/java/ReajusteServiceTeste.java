2e1812f89a657fed72b3715427e5cb09594f73bb
Pedro K. L <pedro.karasudani@hotmail.com>
2023-09-16 15:10:59 -0300

    Refatorando o Teste Reajuste

--- src/test/java/ReajusteServiceTeste.java
+++ src/test/java/ReajusteServiceTeste.java
@@ -10,13 +10,18 @@ import br.com.alura.tdd.modelo.Funcionario;
 import br.com.alura.tdd.service.ReajusteService;
 
 public class ReajusteServiceTeste {
+    ReajusteService service;
+    Funcionario funcionario;
     
+    void inicializar() {
+        this.service = new ReajusteService();
+        this.funcionario = new Funcionario("Pedro", LocalDate.now(), new BigDecimal("1000.00"));
+    }
+
     //Se o desempenho for classificado como "A desejar", o reajuste será de 3% do salário atual.
     @Test
     public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Pedro", LocalDate.now(), new BigDecimal("1000.00"));
-
+        inicializar();        
         service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
 
         assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
@@ -25,9 +30,7 @@ public class ReajusteServiceTeste {
     // Se o desempenho for "Bom,", o reajuste será de 15% do salário.
     @Test
     public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom() {
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Pedro", LocalDate.now(), new BigDecimal("1000.00"));
-
+        inicializar();
         service.concederReajuste(funcionario, Desempenho.BOM);
 
         assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
@@ -36,9 +39,7 @@ public class ReajusteServiceTeste {
     // Se o desempenho for "Ótimo", o reajuste será de 20% do salário.
     @Test
     public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo() {
-        ReajusteService service = new ReajusteService();
-        Funcionario funcionario = new Funcionario("Pedro", LocalDate.now(), new BigDecimal("1000.00"));
-
+        inicializar();
         service.concederReajuste(funcionario, Desempenho.OTIMO);
 
         assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
